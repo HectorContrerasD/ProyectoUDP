47c6af1797e32bca0632ac25808408074449896f
HectorContrerasD <201g0239@rcarbonifera.tecnm.mx>
2024-03-08 21:54:50 -0600

    DTOS a usar

---
+++ AdivinaElBinarioServer/Models/DTOS/UsuarioDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdivinaElBinarioServer.Models.DTOS
+{
+    public class UsuarioDTO
+    {
+        public string Nombre { get; set; } = "";
+        public byte Respuesta { get; set; }
+      
+    }
+}
