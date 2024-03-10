1d2a57b96a35d775c67b77cbc53ddb9fc7b74e57
HectorContrerasD <201g0239@rcarbonifera.tecnm.mx>
2024-03-09 18:23:59 -0600

    Avance

---
+++ AdivinaElBinarioServer/Models/DTOS/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdivinaElBinarioServer.Models.DTOS
+{
+   public class Usuario
+    {
+        public string Nombre { get; set; }
+    }
+}
---
+++ AdivinaElBinarioServer/ViewModels/ServerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdivinaElBinarioServer.ViewModels
+{
+    public class ServerViewModel
+    {
+        Random rnd = new Random();
+        public int NumeroR { get; set; }
+        
+
+    }
+}
