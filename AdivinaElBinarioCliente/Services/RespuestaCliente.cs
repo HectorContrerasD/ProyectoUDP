9d460e3a4a811aaba64a9e742909393c1747fe4a
HectorContrerasD <201g0239@rcarbonifera.tecnm.mx>
2024-03-10 23:43:13 -0600

    Avance cliente

--- AdivinaElBinarioCliente/Services/RespuestaCliente.cs
+++ AdivinaElBinarioCliente/Services/RespuestaCliente.cs
@@ -2,14 +2,17 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace AdivinaElBinarioCliente.Services
 {
     public  class RespuestaCliente
     {
-        private UDPClient cliente = new();
+        private UdpClient cliente = new();
 
         public string Servidor { get; set; } = "0.0.0.0";
 
--- AdivinaElBinarioCliente/ViewModels/RespuestasViewModel.cs
+++ AdivinaElBinarioCliente/ViewModels/RespuestasViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using AdivinaElBinarioCliente.Models.Dtos;
 using AdivinaElBinarioCliente.Services;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
